084accbc23f6270cac6c1c0cb20e6f0a730fb6fa
matanhalfon <matanh1000@gmail.com>
2019-06-06 00:11:52 +0300

    wds

--- data_prce.py
+++ data_prce.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import scipy.stats as stat
 import pandas as pd
 import os
-
-
+import re
+import emoji
+import regex
 
 path="/cs/usr/matanhalfon/Desktop/MLhachton/data"
 
 def readData(path):
     """
-    read connect and shafell the tweets and then write them to
-    train validation and test cvs
+    read connect and shuffle the tweets and then write them to
+    train validation and test cvs in the path
     :param path: a directory with tweet files
+    :return the test part of the data
     """
     data=[]
     for filename in os.listdir(path):
@@ -22,14 +24,41 @@ def readData(path):
             data.append(to_add)
         else:
                 continue
-    addedData=pd.concat(data,sort=True)
+    addedData = pd.concat(data,sort=True)
     result=addedData.sample(frac=1).reset_index(drop=True)
     train=result[:12000]
-    validtion=result[12000:25000]
+    validation=result[12000:25000]
     test=result[25000:]
     train.to_csv(os.path.join(path,r'train.csv'),index=False)
-    validtion.to_csv(os.path.join(path,r'validation.csv'),index=False)
+    validation.to_csv(os.path.join(path,r'validation.csv'),index=False)
     test.to_csv(os.path.join(path,r'test.csv'),index=False)
+    return train
 
 
-print(readData(path))
+
+
+
+
+def extract_emojis(text):
+    decode   = text.decode('utf-8')
+    allchars = [str for str in decode]
+    return [c for c in allchars if c in emoji.UNICODE_EMOJI]
+
+
+
+
+def runMe():
+    data=readData(path)
+    tweets=data["tweet"]
+    data["word count"]=tweets.str.split().apply(len)
+    data["wordl len"]=wordlens=tweets.str.len()
+    data["numCap"]=tweets.str.findall(r'[A-Z]').str.len()
+    data["numHashtags"]=tweets.str.findall(r'#').str.len()
+    data["numOfTaging "]=tweets.str.findall(r'@').str.len()
+    emojilists=tweets.apply(extract_emojis)
+    labels=data["user"]
+    data.drop(["user"],axis=1,inplace=True)
+    return data
+
+
+runMe()
