b4daff90808441712004fe8e91a72c2a0959f4fe
matanhalfon <matanh1000@gmail.com>
2019-06-06 18:15:12 +0300

    wds

--- data_prce.py
+++ data_prce.py
@@ -10,19 +10,47 @@ import regex
 path1="/cs/usr/matanhalfon/Desktop/MLhachton/data"
 trainpath="cs/usr/matanhalfon/PycharmProjects/IMLhack"
 
+# ["wall","crime","border","democrats","china","biden","vp","conan","saw","gameofgames","ellentube"
+#     ,"thankssponsor","nwatch","kkwbeauty","kkwfragrance","shop","lip","kingjames","striveforgreatness"
+#     ,"homie","bro","monsters","joanne","tony","gaga","cr7","cristiano","portugal","nikefootball",
+#  "hello","hala","kimmel","realdonaldtrump","iamguillermo","arnoldsports","great","thank",
+#  "fantastic"]
 
+
+# ["whitehouse","dbongino","joebiden","vp","theellenshow","kkwbeauty","kkwfragrance"
+#     ,"kimkardashian","kuwtk","shop","kingjames","ljfamfoundation","uninterrupted","ladygaga","gaga","lady","btwfoundation"
+#     ,"cr","jimmykimmel","arnold","schwarzenegger"]
 pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 pattern2 = re.compile('@[A-Za-z0-9_-]+ ')
 pattern3 = re.compile('#[A-Za-z0-9_-]+ ')
 
-notRTcommenWords=["wall","crime","border","democrats","china","biden","vp","conan","saw","gameofgames","ellentube"
-,"thankssponsor","nwatch","kkwbeauty","kkwfragrance","shop","lip","kingjames","striveforgreatness"
-,"homie","bro","monsters","joanne","tony","gaga","cr7","cristiano","portugal","nikefootball",
-"hello","hala","kimmel","realdonaldtrump","iamguillermo","arnoldsports","great","thank",
-"fantastic"]
+notRTcommenWords=['border', 'numbers', 'democrats', 'maga', 'crime', 'total', 'witch', 'hunt',
+'military', 'democrat', 'collusion', 'southern', 'fbi', 'report', 'dems', 'economy', 'dollars', 'illegal', 'others',
+'foxnews', 'biden', 'jill', 'joe', 'vice', 'ryan', 'dr', 'middle', 'class', 'tax', 'saw', 'conan', 'ellentube',
+'nwatch', 'clip', 'thankssponsor', 'audience', 'gameofgames', 'splittinguptogether', 'andylassner',
+'thekalenallen', 'official_twitch', 'played', 'kkwbeauty', 'pop', 'pst', 'kkwfragrance', 'sold', 'shop', 'lipstick',
+'kkw', '12pm', 'classic', 'palette', 'lip', 'liners', 'lipsticks', 'nude', 'striveforgreatness', 'lil', 'bro', 'homie',
+'sir', 'brother', 'lol', 'crazy', 'g', 'tony', 'gaga', 'lady', 'joanne', 'monsters', 'training', 'cristiano',
+'portugal', 'hi', 'para', 'mercurial', 'cr7', 'nikefootball', 'madrid', 'el', 'collection', 'hola', 'hala', 'e',
+'match', 'photos', 'cr7underwear', 'football', 'yesterday', 'hello', 'en', 'que', 'de', 'oscars', 'meantweets',
+'realdonaldtrump', 'iamguillermo', 'kimmel', 'thecousinsal', 'trump', 'real', 'old', 'gerrymandering', 'time',
+'get', 'love', 'celebapprentice', 'reform', 'great', 'next', 'friend', 'arnoldsports', 'pumped', 'wait', 'see',
+'take', 'thank', 'together', 'first', 'one', 'know', 'amp', 'every', 'day', 'world', 'like', 'fantastic',
+'asasafterschool', 'back', 'thanks', 'terminator', 'snapchat', 'future', 'join', 'us', 'miss', 'want',
+                  'proud', 'today', 'w', 'support', 'go', 'people', 'watch', 'work', 'let', 'best']
+
+RTcommenWords=['whitehouse', 'dbongino', 'president', 'joebiden', 'vp', 'kkwbeauty', 'available', '12pm', 'pst',
+'kkwfragrance', 'west', 'collection', 'kuwtk', 'kim', 'shop', 'kimkardashian', 'kkwmafia',
+'lipstick', 'palette', 'lip', 'lebron', 'kingjames', 'uninterrupted', 'ljfamfoundation', 'gaga',
+'tony', 'lady', 'ladygaga', 'btwfoundation', 'cristiano', 'jimmykimmel', 'rt', 'arnold',
+'schwarzenegger', 'amp', 'thearnoldfans', 'great', 'http', 'reddit', 'gerrymandering', 'get', 'n', 'today',
+'sabotagemovie', 'snapchat', 'watch', 'live', 'tank', 'new', 'terminator', 'like']
 
 
-RTS="RT @"
+numricfichers=["word count","wordl len","numCap","numCap","numHashtags","numOfTaging ","mean word",
+               "num of !" ,   "num of ?","num of dots","num of commas"]
+
+
 
 def readData(path):
     """
@@ -61,7 +89,7 @@ def getMeanWord(text):
     avg=sum(len(word) for word in words)/len(words)
     return avg
 
-def iscontains(text,word=RTS):
+def iscontains(text,word):
     if word in text:
         return 1
     else:
@@ -71,7 +99,7 @@ def iscontains(text,word=RTS):
 
 def pre_pro (sentence):
     #  remove sites from tweets, @, #
-    sentence = sentence.lower()
+    # sentence = sentence.lower()
     r_site = pattern.sub('', sentence)
     # r_strudel = pattern2.sub('', r_site)
     # r_hesteck = pattern3.sub('', r_strudel)
@@ -88,20 +116,27 @@ def RTsplit(data):
 def writeRawRT(path):
     data=pd.read_csv("train.csv")
     tweets=data["tweet"]
-    data["is RT"]=tweets.apply(isRT)
+    data["is RT"]=tweets.apply(iscontains,word="is RT")
     RT,notRT=RTsplit(data)
     RT = RT.drop(["is RT"],axis = 1)
     notRT = notRT.drop(["is RT"], axis =1)
     RT.to_csv(r'rawRT.csv',index=False)
     notRT.to_csv(r'rawNotRT.csv',index=False)
 
+def addcommenwords(tweets,data,flag):
+    if flag==1:
+        for comword in notRTcommenWords:
+            newcol=tweets.apply(iscontains,word=comword)
+            data[comword]=newcol
+    else:
+        for comword in RTcommenWords:
+            newcol=tweets.apply(iscontains,word=comword)
+            data[comword]=newcol
+    return data
 
 
-def runMe(path):
-    # data=readData(path)
-    data=pd.read_csv("train.csv")
-    tweets=data["tweet"]
-    data.tweet=tweets.apply(pre_pro)
+
+def  addnmricfichers(tweets,data):
     data["word count"]=tweets.str.split().apply(len)
     data["wordl len"]=wordlens=tweets.str.len()
     data["numCap"]=tweets.str.findall(r'[A-Z]').str.len()
@@ -113,14 +148,23 @@ def runMe(path):
     data["num of ?"]=tweets.str.findall(r'\?').str.len()
     data["num of dots"]=tweets.str.findall(r'\.').str.len()
     data["num of commas"]=tweets.str.findall(r'\,').str.len()
-    data["is RT"]=tweets.apply(iscontains)
+    data["is RT"]=tweets.apply(iscontains,word="RT @")
+    cols = list(data.columns)
+    for col in numricfichers:
+        data[col] = (data[col] - data[col].mean())/data[col].std(ddof=0)
+    return data
+
+
+def runMe(path):
+    # data=readData(path)
+    data=pd.read_csv("train.csv")
+    tweets=data["tweet"]
+    data.tweet=tweets.apply(pre_pro)
+    data=addnmricfichers(tweets,data)
     labels=data["user"]
-    # data.drop(["user"],axis=1,inplace=True)
-    # RT,notRT=RTsplit(data)
-    for comword in notRTcommenWords:
-        newcol=tweets.apply(iscontains,word=comword)
-        data[comword]=newcol
     RT ,notRT=RTsplit(data)
+    RT=addcommenwords(tweets,RT,0)
+    notRT=addcommenwords(tweets,notRT,1)
     # RT.to_csv(r'trainRT.csv',index=False)
     # notRT.to_csv(r'trainNotRT.csv',index=False)
     return RT ,notRT
